f3dd0a5331f6b42ca5b7c95ce22493ec2f697a9c
Mindha <mindhaningrum11@gmail.com>
2017-09-06 13:21:17 +0700

    aku rubah lagi tombolnya

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ if(isset($accessToken)){
                 <div class="form-group">
                         <?php echo $output; ?>
                     <a href="#" class="btn btn-danger btn-block" role="button" aria-pressed="true">Google</a>
-                    <a href="#" class="btn btn-info btn-block" role="button" aria-pressed="true">Twitter update</a>         
+                    <a href="#" class="btn btn-info btn-block" role="button" aria-pressed="true">Twitter</a>         
                     </div>
                 </div>                
             </form>
